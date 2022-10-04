01adee223c9ba42b71cf34e7c3f6d7cfd8d978cf
Belonka <98660034+Belonka@users.noreply.github.com>
2022-10-04 06:12:20 +0200

    Add files via upload

---
+++ Homework-2/script.js
@@ -0,0 +1,23 @@
+let userAge = '35',
+userFirstName = 'Alina',
+userSecondName = 'Bilous';
+console.log('userAge', userAge);
+console.log('userFirstName', userFirstName);
+console.log('userSecondName', userSecondName);
+
+let newUser = 'Alan';
+console.log('newUser', newUser);
+let shoppingList = 'tomorrow';
+console.log('shoppingList', shoppingList);
+let userOnline = 'Online';
+console.log('userOnline', userOnline);
+let EmployeeSalary
+let mouseCursor
+
+let message = 'Hello World!';
+console.log('message', message);
+
+
+
+
+ 
